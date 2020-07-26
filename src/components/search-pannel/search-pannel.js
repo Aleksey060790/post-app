11939a2a9ee32bcbb1afb7f96a7d2fb8f411dbb1
Aleksey <as_webdev@ukr.net>
2020-07-26 10:05:56 +0300

    added function of posts searching

--- src/components/search-pannel/search-pannel.js
+++ src/components/search-pannel/search-pannel.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './search-pannel.css'
 
-const SearchPannel = () => {
-    return (
-        <input
-            className="form-control search-input"
-            type="text"
-            placeholder="Поиск по записям"
-        />
-    )
-}
+export default class SearchPannel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            term: ''
+        }
+        this.onUpdateSearch = this.onUpdateSearch.bind(this)
+    }
 
-export default SearchPannel;
+    onUpdateSearch(e) {
+        const term = e.target.value;
+        this.setState({ term });
+        this.props.onUpdateSearch(term);
+
+    }
+
+    render() {
+        return (
+            <input
+                className="form-control search-input"
+                type="text"
+                placeholder="Поиск по записям"
+                onChange={this.onUpdateSearch}
+            />
+        )
+    }
+}
